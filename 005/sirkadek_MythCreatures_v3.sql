9f8f1c43f064a5b7f5cc511ac5896633dfb5a3ab
kkikeda <theikedas01@Scotts-MacBook-Pro.local>
2015-10-13 09:16:17 -0700

    Submitting SELECT statement assignment — KI

--- 005/sirkadek_MythCreatures_v3.sql
+++ 005/sirkadek_MythCreatures_v3.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 12, 2015 at 04:29 PM
+-- Generation Time: Oct 13, 2015 at 12:09 PM
 -- Server version: 5.5.45-cll
 -- PHP Version: 5.4.31
 
@@ -62,22 +62,28 @@ CREATE TABLE IF NOT EXISTS `creature` (
   `mortality` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `adult_height` int(15) NOT NULL,
   `adult_weight` int(15) NOT NULL,
+  `how_born_id` int(50) NOT NULL,
+  `origin_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `creatureName` (`creature_name`)
+  KEY `creatureName` (`creature_name`),
+  KEY `how_born.id` (`how_born_id`),
+  KEY `birth` (`how_born_id`),
+  KEY `how_born_id` (`how_born_id`),
+  KEY `origin_id` (`origin_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `creature`
 --
 
-INSERT INTO `creature` (`id`, `creature_name`, `food_source`, `power`, `venom`, `mortality`, `adult_height`, `adult_weight`) VALUES
-(1, 'Griffin', 'Horses', 'Medicinal healing', '', '', 9, 300),
-(2, 'Sybaris', 'Livestock, people', '', '', '', 20, 175),
-(3, 'Fenrir', '', 'Rapid growth', '', '', 5, 145),
-(4, 'Skoll', 'Sun', '', '', '', 3, 145),
-(5, 'Hati Hroovitnisson', 'Moon', '', '', '', 3, 145),
-(6, 'Loki', '', '', '', '', 4, 175),
-(7, 'Zilant', 'Grass, Virgins, Youth', '', '', '', 6, 200);
+INSERT INTO `creature` (`id`, `creature_name`, `food_source`, `power`, `venom`, `mortality`, `adult_height`, `adult_weight`, `how_born_id`, `origin_id`) VALUES
+(1, 'Griffin', 'Horses', 'Medicinal healing', '', '', 9, 300, 1, 1),
+(2, 'Sybaris', 'Livestock, people', '', '', '', 20, 175, 1, 2),
+(3, 'Fenrir', '', 'Rapid growth', '', '', 5, 145, 2, 3),
+(4, 'Skoll', 'Sun', '', '', '', 3, 145, 2, 3),
+(5, 'Hati Hroovitnisson', 'Moon', '', '', '', 3, 145, 2, 3),
+(6, 'Loki', '', '', '', '', 4, 175, 2, 3),
+(7, 'Zilant', 'Grass, Virgins, Youth', '', '', '', 6, 200, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -119,58 +125,6 @@ INSERT INTO `creature_animal_base_part` (`creature_id`, `part_id`, `animal_base_
 -- --------------------------------------------------------
 
 --
--- Table structure for table `creature_how_born`
---
-
-CREATE TABLE IF NOT EXISTS `creature_how_born` (
-  `creature_id` int(11) NOT NULL,
-  `how_born_id` int(11) NOT NULL,
-  KEY `creature_id` (`creature_id`,`how_born_id`),
-  KEY `how_born_id` (`how_born_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `creature_how_born`
---
-
-INSERT INTO `creature_how_born` (`creature_id`, `how_born_id`) VALUES
-(1, 1),
-(2, 1),
-(3, 2),
-(4, 2),
-(5, 2),
-(6, 2),
-(7, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `creature_origin`
---
-
-CREATE TABLE IF NOT EXISTS `creature_origin` (
-  `creature_id` int(11) NOT NULL,
-  `origin_id` int(11) NOT NULL,
-  KEY `creature_id` (`creature_id`,`origin_id`),
-  KEY `origin_id` (`origin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `creature_origin`
---
-
-INSERT INTO `creature_origin` (`creature_id`, `origin_id`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 3),
-(5, 3),
-(6, 3),
-(7, 4);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `creature_sound`
 --
 
@@ -203,7 +157,8 @@ INSERT INTO `creature_sound` (`creature_id`, `sound_id`) VALUES
 CREATE TABLE IF NOT EXISTS `how_born` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `how_born` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `how_born` (`how_born`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
@@ -350,21 +305,23 @@ CREATE TABLE IF NOT EXISTS `sound` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `year_recorded` year(4) NOT NULL,
   `sound_length` int(5) NOT NULL,
-  PRIMARY KEY (`id`)
+  `sound_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sound_type_id` (`sound_type_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `sound`
 --
 
-INSERT INTO `sound` (`id`, `year_recorded`, `sound_length`) VALUES
-(1, 1987, 45),
-(2, 1994, 30),
-(3, 2006, 45),
-(4, 2001, 60),
-(5, 1994, 90),
-(6, 1987, 45),
-(7, 1982, 15);
+INSERT INTO `sound` (`id`, `year_recorded`, `sound_length`, `sound_type_id`) VALUES
+(1, 1987, 45, 3),
+(2, 1994, 30, 4),
+(3, 2006, 45, 2),
+(4, 2001, 60, 5),
+(5, 1994, 90, 6),
+(6, 1987, 45, 1),
+(7, 1982, 15, 3);
 
 -- --------------------------------------------------------
 
@@ -404,33 +361,6 @@ INSERT INTO `sound_individual_role` (`sound_id`, `role_id`, `individual_id`) VAL
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sound_sound_type`
---
-
-CREATE TABLE IF NOT EXISTS `sound_sound_type` (
-  `sound_id` int(11) NOT NULL,
-  `sound_type_id` int(11) NOT NULL,
-  `season` enum('Winter','Spring','Summer','Autumn') COLLATE utf8_unicode_ci NOT NULL,
-  KEY `sound_id` (`sound_id`,`sound_type_id`),
-  KEY `sound_type_id` (`sound_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `sound_sound_type`
---
-
-INSERT INTO `sound_sound_type` (`sound_id`, `sound_type_id`, `season`) VALUES
-(1, 1, 'Spring'),
-(2, 2, 'Winter'),
-(3, 3, 'Summer'),
-(4, 4, 'Autumn'),
-(5, 5, 'Summer'),
-(6, 6, 'Autumn'),
-(7, 7, 'Spring');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `sound_type`
 --
 
@@ -458,26 +388,19 @@ INSERT INTO `sound_type` (`id`, `sound_type`) VALUES
 --
 
 --
+-- Constraints for table `creature`
+--
+ALTER TABLE `creature`
+  ADD CONSTRAINT `creature_ibfk_2` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`id`),
+  ADD CONSTRAINT `creature_ibfk_1` FOREIGN KEY (`how_born_id`) REFERENCES `creature` (`id`);
+
+--
 -- Constraints for table `creature_animal_base_part`
 --
 ALTER TABLE `creature_animal_base_part`
   ADD CONSTRAINT `creature_animal_base_part_ibfk_3` FOREIGN KEY (`animal_base_id`) REFERENCES `animal_base` (`id`),
   ADD CONSTRAINT `creature_animal_base_part_ibfk_1` FOREIGN KEY (`creature_id`) REFERENCES `creature` (`id`),
   ADD CONSTRAINT `creature_animal_base_part_ibfk_2` FOREIGN KEY (`part_id`) REFERENCES `part` (`id`);
-
---
--- Constraints for table `creature_how_born`
---
-ALTER TABLE `creature_how_born`
-  ADD CONSTRAINT `creature_how_born_ibfk_2` FOREIGN KEY (`how_born_id`) REFERENCES `how_born` (`id`),
-  ADD CONSTRAINT `creature_how_born_ibfk_1` FOREIGN KEY (`creature_id`) REFERENCES `creature` (`id`);
-
---
--- Constraints for table `creature_origin`
---
-ALTER TABLE `creature_origin`
-  ADD CONSTRAINT `creature_origin_ibfk_2` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`id`),
-  ADD CONSTRAINT `creature_origin_ibfk_1` FOREIGN KEY (`creature_id`) REFERENCES `creature` (`id`);
 
 --
 -- Constraints for table `creature_sound`
@@ -494,19 +417,18 @@ ALTER TABLE `relationship`
   ADD CONSTRAINT `relationship_ibfk_2` FOREIGN KEY (`creature_id2`) REFERENCES `creature` (`id`);
 
 --
+-- Constraints for table `sound`
+--
+ALTER TABLE `sound`
+  ADD CONSTRAINT `sound_ibfk_1` FOREIGN KEY (`sound_type_id`) REFERENCES `sound_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `sound_individual_role`
 --
 ALTER TABLE `sound_individual_role`
   ADD CONSTRAINT `sound_individual_role_ibfk_3` FOREIGN KEY (`individual_id`) REFERENCES `individual` (`id`),
   ADD CONSTRAINT `sound_individual_role_ibfk_1` FOREIGN KEY (`sound_id`) REFERENCES `sound` (`id`),
   ADD CONSTRAINT `sound_individual_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
-
---
--- Constraints for table `sound_sound_type`
---
-ALTER TABLE `sound_sound_type`
-  ADD CONSTRAINT `sound_sound_type_ibfk_1` FOREIGN KEY (`sound_id`) REFERENCES `sound` (`id`),
-  ADD CONSTRAINT `sound_sound_type_ibfk_2` FOREIGN KEY (`sound_type_id`) REFERENCES `sound_type` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
