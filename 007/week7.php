55a491be34568fd4efa7f9bcef126f23cdd2c1b9
KimberlySawtelle <kimberly.sawtelle@umit.maine.edu>
2015-11-03 20:08:24 -0500

    Sawtelle Week 7 Assignment
    php files and URL link (in text file) for Week 7 assignment.

---
+++ 007/week7.php
@@ -0,0 +1,28 @@
+<html>
+
+	<head>
+		<title>Week 7 Assignment</title>
+	</head>
+
+	<body>
+
+<?php
+
+	class nameGenerator{
+		private $firstName;
+		private $lastName;
+
+		public function setName($userName){
+		//error checking takes place here
+		$this->name = $userName;
+		}
+
+
+		public function getName(){
+		echo $this->name;
+		}
+	}
+?>
+
+	</body>
+</html>	
