7646ac61e199fea2c367d4cf56f3d2a0c6cf9eeb
Colinjcw <cjcw@myfairpoint.net>
2017-11-16 06:07:03 -0500

    Colin's index.php fiel 008

--- 008/index.php
+++ 008/index.php
@@ -10,9 +10,35 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    error_reporting(E_ALL); 
+    ini_set("display_errors", 1); 
 
-    Insert PHP for displaying album data here!
+    $file_handle = fopen('./albumlist.csv', 'r');
 
+    $first_line = fgetcsv($file_handle);
+
+    for($i=0; $i<count($first_line); $i++){
+        print_r('Column header found: '.$first_line[$i].'<br>');
+    }
+
+    while($data_row = fgetcsv($file_handle)){
+        print_r("<p>This is the #$data_row[0] album:<br>");
+        for($i=1; $i<count($data_row); $i++){
+            if($i < 4){
+                print_r("$first_line[$i]: $data_row[$i]<br>");
+            } else {
+                $genres = str_getcsv($data_row[$i]);
+
+                for($j=0; $j<count($genres); $j++){
+                    print_r("$first_line[$i] #".($j+1)." is $genres[$j]<br>");
+                }
+            }
+        }
+        print_r('</p>');
+    }
+
+    fclose($file_handle);
+?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
