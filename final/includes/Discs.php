f4ea0b605099d0b599ec17e676ba20056e969066
Colinjcw <cjcw@myfairpoint.net>
2017-12-14 15:38:35 -0500

    CW's Final Project

--- final/includes/Discs.php
+++ final/includes/Discs.php
@@ -1,20 +1,20 @@
 <?php
- class Disc{
+ class Album{
      private $ID;
      private $Title;
      private $Year;
-     private $ArtistID;
+     private $Artist_id;
      private $Artist;
-     private $ComposerID;
+     private $Composer_id;
      private $Composer;
-     private $ImageURL;
+     private $Image_url;
 
 
      public function setID($idNumber){
         $this->ID = $idNumber;
      }
      public function getID(){ 
-        print_r( 'ID: '.$this->ID . '<br>'); 
+        print_r( 'ID: '.$this->id . '<br>'); 
      }
      public function setTitle($titleName){ 
         $this->Title = $titleName; 
@@ -39,23 +39,23 @@
      }
      public function getArtist(){ 
         print_r('Artist:'.$this->Artist . '<br>'); }
-    }
-    public function setComposerID($composerNumber){ 
+     }
+     public function setComposerID($composerNumber){ 
         $this->ComposerID = $composerNumber;
      }
-    public function getComposerID(){ 
+     public function getComposerID(){ 
         print_r('ComposerID:'.$this->ComposerID . '<br>'); }
      }
-    public function setComposer($composer){ 
-        $this->Composer = $composer;
+     public function setComposer($composer){ 
+        $this->Composer = str_getcsv($composer);
      } 
-    public function getComposer(){  
+     public function getComposer(){  
         print_r('Composer:'.$this->Composer . '<br>'); }
 
-    public function setImageURL($imageURL){ 
+     public function setImageURL($imageURL){ 
         $this->ImageURL = $imageURL;
      }
-    public function getImageURL(){ 
+     public function getImageURL(){ 
         print_r('ImageURL:'.$this->ImageURL . '<br>'); }
      }   
      public function setData($data_row){
@@ -79,29 +79,62 @@
         $this->getImageURL();
      }
 
+     public function save(){
+        global $pdo;
 
+        try{
+            $disc_insert = $pdo->prepare("INSERT INTO album (ID, Title, Year, Artist_ID, Artist, ImageURL)
+                                            VALUES (?, ?, ? ,?, ?, ?)");
+            $db_album = $album_insert->execute([$this->ID, $this->Title, $this->Year, $this->ArtistID, $this->Artist, $this->imageURL]);
+            $this->ID = $pdo->lastInsertID();
+            print_r("--Saved this title to the database"--<br>\n");
+
+            $select_composer = $pdo->prepare("SELECT * FROM Composer WHERE name = ?");
+            $composer_insert = $pdo->prepare("INSERT INTO Composer (name) VALUES (?);
+            $composer_link = $pdo->prepare("INSERT INTO album-composer (album_id, composer_id) VALUES (?, ?)")
+
+            for($i=0; $i<count($this->composers); $i++){
+                $select_composer->execute([implode(', ',$this_composers)]);
+                $exisiting_composer = select_composer->fetch();
+                if (!$exisiting_composer){
+                    $db_composer = composer_insert->execute([$this->Composer[$i]]);
+                    $Composer_id = $pdo->lastInsertID();
+                    $Composer_id = $existing_composer['id'];
+                }
+                $composer_link->execute([this->id], composer_id);
+                print_r("Connected" . $this->composers[$i]."to$this->Title<br>\n");
+            }
+            flush();
+            ob_flush();
+
+        } catch (PDOException $e){
+            print_r("Error saving album to database: ".$e->getMessage() ."<br>\n")
+            exit;
+
+        }
+     }
  }
 
- $file_handle = fopen('./disc.list.csv', 'r');
+//  $file_handle = fopen('./disc.list.csv', 'r');
 
- $first_line = fgetcsv($file_handle);
+//  $first_line = fgetcsv($file_handle);
 
- for($i=0; $i<count($first_line); $i++){
-     print_r('Column header found: '.$first_line[$i].'<br>');
- }
+//  for($i=0; $i<count($first_line); $i++){
+//      print_r('Column header found: '.$first_line[$i].'<br>');
+//  }
 
- $discs = array();
+//  $albums = array();
  
- while($data_row = fgetcsv($file_handle)){
-    $disc = new Disc();
-    $disc->setData($data_row);
-     array_push($albums, $album);
- }
+//  while($data_row = fgetcsv($file_handle)){
+//     $album = new Album();
+//     $album->setData($data_row);
+//      array_push($albums, $album);
+//  }
 
- for($i=count($discs)-1; $i>=0; $i--){
-     print_r("<p>This is the #$i disc:<br>");
-     print_r('</p>');
- }
+//  for($i=count($albums)-1; $i>=0; $i--){
+//      print_r("<p>This is the #$i album:<br>");
+//      print_r('</p>');
+//  }
 
- fclose($file_handle);
+//  fclose($file_handle);
 ?>
---
+++ final/read_disc.data.php
@@ -0,0 +1,31 @@
+<?php
+    error_reporting(E_ALL); 
+    ini_set("display_errors", 1); 
+    include_once("./includes/db_config.php");
+    include_once("./includes/Discs.php");
+
+    //Open the file
+    $file_handle = fopen('./disclist.csv', 'r');
+
+    //Read the first line (which is the row of headers)
+    $first_line = fgetcsv($file_handle);
+
+    //Print out the headers
+    for($i=0; $i<count($first_line); $i++){
+        print_r('Column header found: '.$first_line[$i].'<br>');
+    }
+
+    //Create an empty array that will be filled with discs
+    $albums = array();
+    
+    //This loop reads through the data file and instantiates Album objects for each row
+    //It stores these objects in the $albums array
+    while($data_row = fgetcsv($file_handle)){
+        $album = new Album();
+        $album->setData($data_row);
+        array_push($albums, $album);
+    }
+
+    //Close the file
+    fclose($file_handle);
+?>
