b5dc1ae869794445540bb1144bfd1f04655e80c6
amblack540 <55031670+amblack540@users.noreply.github.com>
2019-12-16 23:44:37 -0500

    finished saving data into DB

--- Final AB/includes/Pokemon.php
+++ Final AB/includes/Pokemon.php
@@ -18,9 +18,18 @@
     public function getPokedex(){ print_r(''.$this->pokedex . '<br>'); }
     public function setSpecies($speciesName){$this->species = $speciesName;}
     public function getSpecies(){print_r('Species: '.$this->species . '<br>');}
-    public function setBuddyCandy($buddyCandyDistance){ $this->buddy_candy = $buddyCandyDistance; }
-    public function getBuddyCandy(){ print_r('Walk this many km to earn a candy: '.$this->buddy_candy . '<br>'); }
    
+    // the old version
+    // public function setBuddyCandy($buddyCandyDistance){ $this->buddy_candy = $buddyCandyDistance; }
+    // public function getBuddyCandy(){ print_r('Walk this many km to earn a candy: '.$this->buddy_candy . '<br>'); }
+    public function setBuddyCandy($buddyCandyDistance){  
+        $this->buddy_candy = str_getcsv($buddyCandyDistance); 
+     }
+     public function getBuddyCandy() {
+     for($j=0; $j<count($this->buddy_candy); $j++){
+         print_r("<span style='color:indigo'>Walk this many km to earn a candy: ".$this->buddy_candy[$j]."</span><br>"); }  }  
+
+
     // the old version 
     // public function setEvolveCandy($evolveCandy){ $this->evolve_candy = $evolveCandy; }
     // public function getEvolveCandy(){print_r('Candies Needed to Evolve: '.$this->evolve_candy . '<br>');}
@@ -30,15 +39,25 @@
      }
      public function getEvolveCandy() {
      for($j=0; $j<count($this->evolve_candy); $j++){
-         print_r("<span style='color:purple'>Candies Needed to Evolve:".$this->evolve_candy[$j]."</span><br>"); }  }
+         print_r("<span style='color:purple'>Candies Needed to Evolve: ".$this->evolve_candy[$j]."</span><br>"); }  }
     
     public function setEvolvesFrom($evolvesFrom){ $this->evolve_from = $evolvesFrom; }
     public function getEvolvesFrom(){ print_r( 'Evolves From: '.$this->evolve_from . '<br>'); }
     public function setEvolvesTo($evolvesTo){ $this->evolve_to = $evolvesTo; }
     public function getEvolvesTo(){ print_r( 'Evolves To: '.$this->evolve_to . '<br>'); }
-    public function setRegion($regionName){ $this->region = $regionName; }
-    public function getRegion(){ print_r( 'Region First Discoverd: '.$this->region . '<br>'); }
-    
+  
+    // old version
+    // public function setRegion($regionName){ $this->region = $regionName; }
+    // public function getRegion(){ print_r( 'Region First Discoverd: '.$this->region . '<br>'); }
+   
+    public function setRegion($regionName){  
+        $this->region = str_getcsv($regionName); 
+     }
+     public function getRegion() {
+     for($j=0; $j<count($this->region); $j++){
+         print_r("<span style='color:orange'>Region: ".$this->region[$j]."</span><br>"); }  }
+
+    // old version
     // public function setGeneration($generationNumber){ $this->generation = $generationNumber; }
     // public function getGeneration(){ print_r( 'Generation: '.$this->generation . '<br>'); }
     public function setGeneration($generationNumber){  
@@ -46,9 +65,8 @@
      }
      public function getGeneration() {
      for($j=0; $j<count($this->generation); $j++){
-         print_r("<span style='color:green'>".$this->generation[$j]."</span><br>"); }  }
+         print_r("<span style='color:green'>Generation: ".$this->generation[$j]."</span><br>"); }  }
 
-  
     public function setExdt($exdtValue){ $this->exdt = $exdtValue; }
     public function getExdt(){ print_r('Special Information: '.$this->exdt . '<br>'); }  
    
@@ -61,7 +79,6 @@
             else print_r("<span style='color:red'>Type #".($j+1)." is ".$this->type[$j]."</span><br>");
         }
     }
-
 
     public function setData($data_row){
         // this is a function that pulls up other functions and puts them together. But it also passes in the data   
@@ -98,13 +115,12 @@
         global $pdo;
 
         try{
-       // between here and the end of connecting types, I have it working as of 1:34am on december 16. Need to rework tables and get the rest of the code working!    
+       // Successfully entering pokemon, candy, and generation data so far 12/16 10:17pm    
            $pokemon_insert = $pdo->prepare("INSERT INTO pokemon (pokemon_pokedex, pokemon_species, pokemon_evolves_from, pokemon_evolves_to, pokemon_exdt)
                                             VALUES (?, ?, ?, ?, ?)");
             $db_pokemon = $pokemon_insert->execute([$this->pokedex, $this->species, $this->evolve_from, 
             $this->evolve_to, $this->exdt]);
-            // implode(',', $this->exdt)]);
-            // i need to go back and figure out more about the implode thing and what it does. what variable i need to be in there, if any
+
             $this->id = $pdo->lastInsertId();
             print_r("--Saved $this->species to the database.--<br>\n");
 
@@ -124,7 +140,7 @@
                 $type_link->execute([$this->id, $type_id]);
                 print_r("Connected ".$this->type[$i]." to $this->species<br>\n");
 
-        //now enter data into the candy table and link it to pokemon
+        //enter data into the candy table and link it to pokemon
             $select_candy = $pdo->prepare("SELECT * FROM candy WHERE candy_to_evolve = ?");
             $candy_insert = $pdo->prepare("INSERT INTO candy (candy_to_evolve) VALUES (?)");
             $candy_link = $pdo->prepare("INSERT INTO candy_pokemon (pokemon_id, candy_id) VALUES (?, ?)");
@@ -140,10 +156,11 @@
                  }
                  $candy_link->execute([$this->id, $candy_id]);
                  print_r("Connected ".$this->evolve_candy[$j]." to $this->species<br>\n");
-                 
+        
+        // enter data into the generation table and link it to pokemon      
             $select_generation = $pdo->prepare("SELECT * FROM generation WHERE generation_name = ?");
             $generation_insert = $pdo->prepare("INSERT INTO generation (generation_name) VALUES (?)");
-            $generation_link = $pdo->prepare("INSERT INTO pokemon (generation_id) VALUES (?)");
+            $generation_link = $pdo->prepare("INSERT INTO pokemon_generation (pokemon_id, generation_id) VALUES (?, ?)");
                 for($i=0; $i<count($this->generation); $i++){
                 $select_generation->execute([$this->generation[$i]]);
                 $existing_generation = $select_generation->fetch();
@@ -154,54 +171,55 @@
                      $generation_id = $existing_generation['generation_id'];
                  }
                  $generation_link->execute([$this->id, $generation_id]);
-                 print_r("Connected ".$this->generation[$i]." to $this->species<br>\n");
+                 print_r("Connected ".$this->generation[$i]." to $this->species<br>\n"); 
+           
+           
+         // enter data into the region table and link it to pokemon           
+            $select_region = $pdo->prepare("SELECT * FROM region WHERE region_name = ?");
+            $region_insert = $pdo->prepare("INSERT INTO region (region_name) VALUES (?)");
+            $region_link = $pdo->prepare("INSERT INTO pokemon_region (pokemon_id, region_id) VALUES (?, ?)");
+                for($i=0; $i<count($this->region); $i++){
+                $select_region->execute([$this->region[$i]]);
+                $existing_region = $select_region->fetch();
+                if(!$existing_region){
+                    $db_region = $region_insert->execute([$this->region[$i]]);
+                    $region_id = $pdo->lastInsertID();
+                } else {
+                    $region_id = $existing_region['region_id'];
+                }
+                $region_link->execute([$this->id, $region_id]);
+                print_r("Connected ".$this->region[$i]." to $this->species<br>\n");              
+      
+         // enter data into the buddy_distance_rate table and link it to pokemon            
+            $select_buddy_distance_rate = $pdo->prepare("SELECT * FROM buddy_distance_rate WHERE buddy_candy_rate = ?");
+            $buddy_distance_rate_insert = $pdo->prepare("INSERT INTO buddy_distance_rate (buddy_candy_rate) VALUES (?)");
+            $buddy_distance_rate_link = $pdo->prepare("INSERT INTO pokemon_buddy_distance_rate (pokemon_id, buddy_id) VALUES (?, ?)");
+                for($i=0; $i<count($this->buddy_candy); $i++){
+                $select_buddy_distance_rate->execute([$this->buddy_candy[$i]]);
+                $existing_buddy_distance_rate = $select_buddy_distance_rate->fetch();
+                 if(!$existing_buddy_distance_rate){
+                     $db_buddy_distance_rate = $buddy_distance_rate_insert->execute([$this->buddy_candy[$i]]);
+                     $buddy_id = $pdo->lastInsertID();
+                 } else {
+                     $buddy_id = $existing_buddy_distance_rate['buddy_id'];
+                 }
+                 $buddy_distance_rate_link->execute([$this->id, $buddy_id]);
+                 print_r("Connected ".$this->buddy_candy[$i]." to $this->species<br>\n");
+                 }                  
+        
                 
-            
-                    
-    //the code above seems to work a little, although with errors. It is after this point that things get completely derailed. What do I need to fix?
-
-
-            // $select_region = $pdo->prepare("SELECT * FROM region WHERE region_name = ?");
-            // $region_insert = $pdo->prepare("INSERT INTO region (region_name) VALUES (?)");
-            // $region_link = $pdo->prepare("INSERT INTO pokemon (pokemon_id, region_id) VALUES (?, ?)");
-            // for($i=0; $i<count($this->region); $i++){
-            //     $select_region->execute([$this->region[$i]]);
-            //     $existing_region = $select_region->fetch();
-            //     if(!$existing_region){
-            //         $db_region = $region_insert->execute([$this->region[$i]]);
-            //         $region_id = $pdo->lastInsertID();
-            //     } else {
-            //         $region_id = $existing_region['id'];
-            //     }
-            //     $region_link->execute([$this->id, $region_id]);
-            //     print_r("Connected ".$this->region[$i]." to $this->species<br>\n");
-            //     }
-
-            // $select_buddy_distance_rate = $pdo->prepare("SELECT * FROM buddy_distance_rate WHERE buddy_candy_rate = ?");
-            // $buddy_distance_rate_insert = $pdo->prepare("INSERT INTO buddy_distance_rate (buddy_candy_rate) VALUES (?)");
-            // $buddy_distance_rate_link = $pdo->prepare("INSERT INTO pokemon (pokemon_id, buddy_id) VALUES (?, ?)");
-            // for($i=0; $i<count($this->buddy_distance_rate); $i++){
-            //     $select_buddy_distance_rate->execute([$this->buddy_distance_rate[$i]]);
-            //     $existing_buddy_distance_rate = $select_buddy_distance_rate->fetch();
-            //     if(!$existing_buddy_distance_rate){
-            //         $db_buddy_distance_rate = $buddy_distance_rate_insert->execute([$this->buddy_distance_rate[$i]]);
-            //         $buddy_id = $pdo->lastInsertID();
-            //     } else {
-            //         $buddy_id = $existing_buddy_distance_rate['id'];
-            //     }
-            //     $region_link->execute([$this->id, $buddy_id]);
-            //     print_r("Connected ".$this->buddy_distance_rate[$i]." to $this->species<br>\n");
-            //     }   
-
+                
+                }  
+        // This is giving me the error message, but I am not going to deal with it right now
+        // Notice: ob_flush(): failed to flush buffer. No buffer to flush in /home/abdignet/public_html/pokemon_data/includes/Pokemon.php on line 217
 
             // flush();
-            // ob_flush();
-            
-         catch (PDOException $e){
+            //  ob_flush();           
+   }}}} catch (PDOException $e){
             print_r("Error saving Pokemon to database: ".$e->getMessage() . "<br>\n");
             exit;
         }
  }
- }
+}
 
 ?>
