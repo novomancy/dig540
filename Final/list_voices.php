464be6dc864e3baf160217ba84668a6673e2ecd7
DRamer18 <donna.ramer@maine.edu>
2019-12-20 23:01:14 -0600

    Adding submissions for final

--- Final/list_voices.php
+++ Final/list_voices.php
@@ -19,7 +19,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Voice of Mobile!</title>
+    <title>My Southern Voices!</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
