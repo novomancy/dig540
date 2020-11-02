737547d9666b623150978134d3fd10238e535ca2
DIG540mikejoseph <70819471+DIG540mikejoseph@users.noreply.github.com>
2020-11-02 17:24:12 -0500

    Update index.php
    Updating index.php code

--- 08/index.php
+++ 08/index.php
@@ -10,8 +10,64 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    
 
-    Insert PHP for displaying album data here!
+<?php
+       {
+      error_reporting(E_ALL); 
+    ini_set("display_errors", 1); 
+
+    $file_handle = fopen('./albumlist.csv', 'r');
+
+    $first_line = fgetcsv($file_handle);
+
+    for($i=0; $i<count($first_line); $i++){
+	    
+        print_r('<div> Column header found: '.$first_line[$i].'<br></div>');
+    }
+
+    while($data_row = fgetcsv($file_handle)){
+        print_r("<p><strong>This is the #$data_row[0] album:</strong><br>");
+        for($i=1; $i<count($data_row); $i++){
+            if($i < 4){
+                print_r("$first_line[$i]: $data_row[$i]<br>");
+            } else {
+                $genres = str_getcsv($data_row[$i]);
+
+                for($j=0; $j<count($genres); $j++){
+                 
+	             if($j % 2 == 0){
+?>
+       <span style = "color:black;"> <span>
+<?php		             
+		         print_r("$first_line[$i] #".($j+1)." is $genres[$j]<br>"); }
+	             
+		         else {
+?>    
+      <span style = "color:blue;"> <span>
+      
+<?php      
+      		             
+		         print_r("$first_line[$i] #".($j+1)." is $genres[$j]<br>"); }} 
+	           
+             	             
+            
+           }                   
+         }
+        
+    		
+                
+    }
+        
+		 print_r('</p>');             
+        
+        
+    }
+
+
+    fclose($file_handle);
+
+?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
