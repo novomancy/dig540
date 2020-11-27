cdba991b73ff9280ad34205b2f96346c73d494ea
Alexander Clark <70975983+ajc23510@users.noreply.github.com>
2020-11-27 10:18:34 -0500

    Add files via upload
    http://ajc23510.com/Album_Data/Alexander%20Clark%20-%20Week%20nine%20parsealbum.php

---
+++ Alexander Clark - Week nine parsealbum.php
@@ -0,0 +1,104 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    class Album{
+        private $artist;
+        private $title;
+        private $rank;
+        private $year;
+        private $genres;
+        private $subgenres;
+
+        public function setArtist($artistName){ $this->artist = $artistName; }
+        public function getArtist(){ print_r( 'Artist: '.$this->artist . '<br>'); }
+        public function setTitle($titleName){ $this->title = $titleName; }
+        public function getTitle(){ print_r('Title: '.$this->title . '<br>'); }
+        public function setRank($rankNumber){ $this->rank = $rankNumber; }
+        public function getRank(){ print_r('Rank: '.$this->rank . '<br>'); }
+        public function setYear($releaseYear){ $this->year = $releaseYear; }
+        public function getYear(){ print_r('Year: '.$this->year . '<br>'); }
+        public function setGenres($genres){
+            $this->genres = str_getcsv($genres);
+        }
+        public function getGenres(){
+            for($j=0; $j<count($this->genres); $j++){
+                if($j%2==0) print_r("<span style='color:blue'>Genre #".($j+1)." is ".$this->genres[$j]."</span><br>");
+                else print_r("<span style='color:red'>Genre #".($j+1)." is ".$this->genres[$j]."</span><br>");
+            }
+
+            print_r($this->genres);
+        }
+        public function setSubgenres($subs){
+            $this->subgenres = str_getcsv($subs);
+        }
+        public function getSubgenres(){
+            for($j=0; $j<count($this->subgenres); $j++){
+                if($j%2==0) print_r("<span style='color:green'>Subgenre #".($j+1)." is ".$this->subgenres[$j]."</span><br>");
+                else print_r("<span style='color:pink'>Subgenre #".($j+1)." is ".$this->subgenres[$j]."</span><br>");
+            }
+        }
+
+        public function setData(){
+          $this->artist = $artistName;
+          $this->title = $titleName;
+          $this->rank = $rankNumber;
+          $this->year = $releaseYear;
+        }
+
+        public function getData(){
+          $this->getArtist();
+          $this->getYear();
+          $this->getTitle();
+          $this->getRank();
+          $this->getGenres();
+          $this->getSubgenres();
+        }
+
+
+    }
+
+    $file_handle = fopen('./AlbumList.csv', 'r');
+
+    $first_line = fgetcsv($file_handle);
+
+    for($i=0; $i<count($first_line); $i++){
+        print_r('Column header found: '.$first_line[$i].'<br>');
+    }
+
+    $albums = array();
+
+    while($data_row = fgetcsv($file_handle)){
+        $album = new Album();
+        $album->setArtist($data_row[3]);
+        $album->setTitle($data_row[2]);
+        $album->setRank($data_row[0]);
+        $album->setYear($data_row[1]);
+        $album->setGenres($data_row[4]);
+        $album->setSubgenres($data_row[5]);
+        array_push($albums, $album);
+    }
+
+    for($i=count($albums)-1; $i>=0; $i--){
+        print_r("<p>This is the #$i album:<br>");
+
+        $albums[$i]->getData();
+
+         $albums[$i]->getTitle();
+         $albums[$i]->getYear();
+         $albums[$i]->getRank();
+         $albums[$i]->getArtist();
+         $albums[$i]->getGenres();
+         $albums[$i]->getSubgenres();
+        print_r('</p>');
+    }
+
+    fclose($file_handle);
+
+
+
+
+
+
+
+?>
